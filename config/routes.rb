987af3f5ebf8f1395d1ff27287db71c71ff2fb0f
OmarMWarraich <earthling986@gmail.com>
2024-05-01 11:18:43 +0500

    feat: Upload Cover Images w/ Active Storage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :blog_posts
+  resources :blog_posts do
+    resource :cover_image, only: [:destroy], module: :blog_posts
+  end
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
